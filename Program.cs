4cc386e77d08adaf54d538b743ed9e7e2d5373ce
James Eagle <jimeagle@gmail.com>
2018-05-29 21:51:27 +0100

    Save a last change number file

--- Program.cs
+++ Program.cs
@@ -5,18 +5,27 @@ namespace SteamProxy
 {
     internal static class ChangeFetcher
     {
+        private const string LastChangeFile = "last-changenumber.txt";
+
         private static void Main(string[] args)
         {
-            var steam = new Steam();
+            //var steam = new Steam();
 
-            if (File.Exists("last-changenumber.txt"))
+            if (File.Exists(LastChangeFile))
             {
-                steam.PreviousChangeNumber = uint.Parse(File.ReadAllText("last-changenumber.txt"));
+                //steam.PreviousChangeNumber = uint.Parse(File.ReadAllText("last-changenumber.txt"));
+                Console.WriteLine("file");
             }
-            
+            else
+            {
+                Console.WriteLine("no file");
+            }
+
+            File.WriteAllText(LastChangeFile, "123");
+
             //steam.Connect();
 
-            Console.WriteLine(steam.PreviousChangeNumber);
+            //Console.WriteLine(steam.PreviousChangeNumber);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# reddit-go
+# steam-proxy
+
+An app to fetch information from Steam and send the data to Rabbit.
