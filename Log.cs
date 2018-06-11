eca0f20e68be1d35c74bf6fc6df22612b4cc0b77
James Eagle <jimeagle@gmail.com>
2018-06-11 21:56:31 +0100

    Remove dupe env call

--- Log.cs
+++ Log.cs
@@ -13,6 +13,24 @@ namespace SteamUpdater
 
         private static readonly LoggingServiceV2Client googleCLient = LoggingServiceV2Client.Create();
 
+        public static void GoogleInfo(string message)
+        {
+            Console.WriteLine(message);
+
+            var googleProject = Environment.GetEnvironmentVariable("STEAM_GOOGLE_PROJECT");
+
+            var logName = new LogName(googleProject, "steam-updater-" + env);
+            var logEntry = new LogEntry
+            {
+                LogName = logName.ToString(),
+                Severity = LogSeverity.Info,
+                TextPayload = message
+            };
+            var resource = new MonitoredResource {Type = "global"};
+
+            googleCLient.WriteLogEntries(LogNameOneof.From(logName), resource, null, new[] {logEntry});
+        }
+
         public static void setupRollbar()
         {
             var rollbarKey = Environment.GetEnvironmentVariable(key);
@@ -37,25 +55,6 @@ namespace SteamUpdater
         public static void RollbarInfo(string message)
         {
             RollbarLocator.RollbarInstance.Info(message);
-        }
-
-        public static void GoogleInfo(string message)
-        {
-            Console.WriteLine(message);
-
-            var googleProject = Environment.GetEnvironmentVariable("STEAM_GOOGLE_PROJECT");
-            var envi = Environment.GetEnvironmentVariable("ENV");
-
-            var logName = new LogName(googleProject, "steam-updater-" + envi);
-            var logEntry = new LogEntry
-            {
-                LogName = logName.ToString(),
-                Severity = LogSeverity.Info,
-                TextPayload = message
-            };
-            var resource = new MonitoredResource {Type = "global"};
-
-            googleCLient.WriteLogEntries(LogNameOneof.From(logName), resource, null, new[] {logEntry});
         }
     }
 }
