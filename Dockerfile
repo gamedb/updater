26e46df4c9304146d54cc9fc136df2e6ed89f683
James Eagle <jimeagle@gmail.com>
2018-07-09 18:03:51 +0100

    Add build container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
-FROM microsoft/dotnet
-COPY bin/Release/netcoreapp2.0/osx-x64/publish/SteamUpdater /SteamUpdater
+# Build image
+FROM microsoft/dotnet:2.1-sdk AS build-env
+WORKDIR /app
+
+COPY *.csproj ./
+RUN dotnet restore
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Runtime image
+FROM microsoft/dotnet:2.1-runtime
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+# Needs to be synced!
 COPY last-changenumber.txt /last-changenumber.txt
-EXPOSE 8087
 CMD ["/SteamUpdater"]
--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ git fetch origin
 git reset --hard origin/master
 
 echo "### Building"
-#dotnet publish --runtime osx-x64 --self-contained --configuration release
-dotnet publish --runtime debian-x64 --self-contained --configuration release
+docker build -t steam-updater .
+docker run -p 9090:80 --name "steam-updater" --restart="unless-stopped" --rm steam-updater
 
 echo "### Talking to Rollbar"
 curl https://api.rollbar.com/api/1/deploy/ \
@@ -18,7 +18,9 @@ curl https://api.rollbar.com/api/1/deploy/ \
   -F local_username=Jleagle \
   --silent > /dev/null
 
-echo "### Restarting"
-/etc/init.d/steam-updater restart # Needs pkill SteamUpdater
+#echo "### Restarting"
+#/etc/init.d/steam-updater restart # Needs pkill SteamUpdater
 
+#dotnet publish --runtime osx-x64 --self-contained --configuration release
+#dotnet publish --runtime debian-x64 --self-contained --configuration release
 # Run /steam-updater/bin/Release/netcoreapp2.0/osx-x64/publish/SteamUpdater
