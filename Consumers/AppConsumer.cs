1c105c46d6ee759e64582eefb1382a45baf870d5
James Eagle <jimeagle@gmail.com>
2019-02-28 22:43:14 +0000

    Fix missing json properties

--- Consumers/AppConsumer.cs
+++ Consumers/AppConsumer.cs
@@ -11,7 +11,7 @@ namespace Updater.Consumers
         protected override async Task HandleMessage(BaseMessage payload)
         {
             // Remove any keys that can't be deserialised
-            var json = JObject.Parse(payload.Message.ToString());
+            var json = JObject.Parse(JsonConvert.SerializeObject(payload));
             var key = json.Property("PICSAppInfo");
             key?.Remove();
 
--- Consumers/ProfileConsumer.cs
+++ Consumers/ProfileConsumer.cs
@@ -12,7 +12,7 @@ namespace Updater.Consumers
         protected override async Task HandleMessage(BaseMessage payload)
         {
             // Remove any keys that can't be deserialised
-            var json = JObject.Parse(payload.Message.ToString());
+            var json = JObject.Parse(JsonConvert.SerializeObject(payload));
             var key = json.Property("PICSProfileInfo");
             key?.Remove();
 
